43b64514e7499399a6f382d33e728d36d1149e40
hashemsayel <hashemsayel4@gmail.com>
2025-01-03 15:08:20 +0300

    Populargames section  react

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "swiper/swiper-bundle.min.css";
+
+
+import Home from "./pages/Home/Home";
+import Header from './components/Header/Header';
+import Signin from './pages/SignIn/Signin';
+import Aboutus from "./pages/AboutUs/Aboutus";
+import Footer from "./components/Footer/Footer";
+import Populargames from "./components/Populargames/Populargames"
+
+// Don't change anything in the App function without notifying the whole team.
+
+function App() {
+
+  return( 
+    <>
+    <Header />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />}> 
+              <Route path="games" element={<Home />} />
+            </Route>
+            <Route path="/signin" element={<Signin />}/>
+            <Route path="/aboutus" element={<Aboutus />}/>
+        </Routes>
+        <Populargames/>
+    <Footer />
+    </>
+
+    );
+
+
+}
+
+export default App;
---
+++ components/Populargames/Populargames.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/swiper-bundle.min.css';
+import { Navigation, Pagination, Autoplay } from "swiper";
+
+function Populargames() {
+  return (
+    <section className="Popular-games">
+      <h1>Most Popular</h1>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={5}
+        spaceBetween={30}
+        loop={true}
+        navigation={true}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+      
+      
+      >
+     <SwiperSlide>
+          <img src="image/fifa.jpg" alt="fifa"/>
+          <div className="game-info">
+            <h3 className="game-title">Fifa</h3>
+            <p className="game-category">sport</p>
+            <div className="game-rating">
+              <span>7.7</span>
+              <i className="bx bxs-star bx-tada bx-rotate-270"></i>
+            </div>
+          </div>
+    </SwiperSlide>
+
+
+   <SwiperSlide>
+    <img src="image/battlefield.jpg" alt="Battlefield"/>
+    <div className="game-info">
+      <h3 className="game-title">Battlefield</h3>
+      <p className="game-category">shooter</p>
+      <div className="game-rating">
+        <span>8.7</span>
+        <i className="bx bxs-star bx-tada bx-rotate-270"></i>
+      </div>
+    </div>
+  </SwiperSlide>
+
+  <SwiperSlide>
+    <img src="image/fortnite.jpg" alt="Fortnite"/>
+    <div className="game-info">
+      <h3 className="game-title">Fortnite</h3>
+      <p className="game-category">survival</p>
+      <div className="game-rating">
+        <span>8.3</span>
+        <i className="bx bxs-star bx-tada bx-rotate-270"></i>
+      </div>
+    </div>
+  </SwiperSlide>
+
+  <SwiperSlide>
+    <img src="image/forza.jpg" alt="Forza"/>
+    <div className="game-info">
+      <h3 className="game-title">Forza</h3>
+      <p className="game-category">Sport</p>
+      <div className="game-rating">
+        <span>9.2</span>
+        <i className="bx bxs-star bx-tada bx-rotate-270"></i>
+      </div>
+    </div>
+  </SwiperSlide>
+
+  <SwiperSlide>
+    <img src="image/rdr2.jpg" alt="Red Dead Redemption 2"/>
+    <div className="game-info">
+      <h3 className="game-title">Red Dead Redemption 2</h3>
+      <p className="game-category">open-world</p>
+      <div className="game-rating">
+        <span>8.7</span>
+        <i className="bx bxs-star bx-tada bx-rotate-270"></i>
+      </div>
+    </div>
+  </SwiperSlide>
+
+
+
+
+
+
+      </Swiper>
+    </section>
+  );
+}
+
+export default Populargames;
+
