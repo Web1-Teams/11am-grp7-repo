be12a604a76282ae2189290377624b67cc01e7d9
hashemsayel <hashemsayel4@gmail.com>
2025-01-04 17:15:00 +0300

    Populargames section finish

--- App.js
+++ App.js
@@ -1,39 +1,65 @@
+
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-
 import Home from "./pages/Home/Home";
 import Header from './components/Header/Header';
 import Signin from './pages/SignIn/Signin';
 import Aboutus from "./pages/AboutUs/Aboutus";
 import Footer from "./components/Footer/Footer";
-import Populargames from "./components/Populargames/Populargames";
+import Populargames from "./components/Populargames/Populargames"; // استيراد مكون Populargames
 
 // Don't change anything in the App function without notifying the whole team.
-
 function App() {
+  const games = [
+    { 
+      image: "image/fortnite.jpg", 
+      name: "Fortnite", 
+      genre: "survival", 
+      rating: "9.5/10" 
+    },
+    { 
+      image: "image/rdr2.jpg", 
+      name: "Rdr2", 
+      genre: "survival", 
+      rating: "9.1/10" 
+    },
+    { 
+      image: "image/battlefield.jpg", 
+      name: "Battlefield", 
+      genre: "war", 
+      rating: "9.9/10" 
+    },
+    { 
+      image: "image/apex.jpg", 
+      name: "Apex Legends", 
+      genre: "survival", 
+      rating: "9.9/10" 
+    },
+    { 
+      image: "image/forza.jpg", 
+      name: "Forza", 
+      genre: "racing", 
+      rating: "9.6/10" 
+    }
+  ];
 
-  return( 
+  return (
     <>
-    <Header />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />}> 
+          <Route path="games" element={<Home />} />
+        </Route>
+        <Route path="/signin" element={<Signin />}/>
+        <Route path="/aboutus" element={<Aboutus />}/>
+      </Routes>
+      
+      <Populargames games={games} /> 
 
-        <Routes>
-
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />}> 
-              <Route path="games" element={<Home />} />
-            </Route>
-            <Route path="/signin" element={<Signin />}/>
-            <Route path="/aboutus" element={<Aboutus />}/>
-        </Routes>
-        <Populargames/>
-
-       
-    <Footer />
+      <Footer />
     </>
-
-    );
-
-
+  );
 }
 
 export default App;
--- components/Populargames/Populargames.js
+++ components/Populargames/Populargames.js
@@ -1,81 +1,38 @@
 import React, { useState, useEffect } from "react";
 import styles from './Populargames.module.css';
 
-function Populargames() {
+function Populargames({ games }) {  
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % 5);
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % games.length);   
     }, 3000);
 
-    return () => clearInterval(interval); 
-  }, []);
+    return () => clearInterval(interval);
+  }, [games.length]);   
 
   return (
     <section className={styles.Populargames}>
       <h2>Most Popular</h2>
 
       <div className={styles.PopularCards}>
-        <div className={`${styles.PopularCard} ${currentIndex === 0 ? styles.active : ''}`}>
-          <div className={styles.Populartext}>
-            <img src="image/fortnite.jpg" alt="Fortnite" />
-            <h3>Fortnite</h3>
-            <span>survival</span>
-            <div className={styles.rating}>
-              <span>9.5/10</span>
-              <i className='bx bxs-star bx-flashing bx-flip-horizontal'></i>
+        {games.map((game, index) => (  
+          <div
+            key={index}
+            className={`${styles.PopularCard} ${currentIndex === index ? styles.active : ''}`}
+          >
+            <div className={styles.Populartext}>
+              <img src={game.image} alt={game.name} />
+              <h3>{game.name}</h3>
+              <span>{game.genre}</span>
+              <div className={styles.rating}>
+                <span>{game.rating}</span>
+                <i className='bx bxs-star bx-flashing bx-flip-horizontal'></i>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className={`${styles.PopularCard} ${currentIndex === 1 ? styles.active : ''}`}>
-          <div className={styles.Populartext}>
-            <img src="image/rdr2.jpg" alt="Rdr2" />
-            <h3>Rdr2</h3>
-            <span>survival</span>
-            <div className={styles.rating}>
-              <span>9.1/10</span>
-              <i className='bx bxs-star bx-flashing bx-flip-horizontal'></i>
-            </div>
-          </div>
-        </div>
-
-        <div className={`${styles.PopularCard} ${currentIndex === 2 ? styles.active : ''}`}>
-          <div className={styles.Populartext}>
-            <img src="image/battlefield.jpg" alt="Battlefield" />
-            <h3>Battlefield</h3>
-            <span>war</span>
-            <div className={styles.rating}>
-              <span>9.9/10</span>
-              <i className='bx bxs-star bx-flashing bx-flip-horizontal'></i>
-            </div>
-          </div>
-        </div>
-
-        <div className={`${styles.PopularCard} ${currentIndex === 3 ? styles.active : ''}`}>
-          <div className={styles.Populartext}>
-            <img src="image/apex.jpg" alt="Apex Legends" />
-            <h3>Apex Legends</h3>
-            <span>survival</span>
-            <div className={styles.rating}>
-              <span>9.9/10</span>
-              <i className='bx bxs-star bx-flashing bx-flip-horizontal'></i>
-            </div>
-          </div>
-        </div>
-
-        <div className={`${styles.PopularCard} ${currentIndex === 4 ? styles.active : ''}`}>
-          <div className={styles.Populartext}>
-            <img src="image/forza.jpg" alt="Forza" />
-            <h3>Forza</h3>
-            <span>racing</span>
-            <div className={styles.rating}>
-              <span>9.6/10</span>
-              <i className='bx bxs-star bx-flashing bx-flip-horizontal'></i>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
