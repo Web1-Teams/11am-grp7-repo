1b5011e98c9ab41f0e5843f8ccca743aea389325
hashemsayel <hashemsayel4@gmail.com>
2025-01-04 17:30:24 +0300

    Footer responsive Populargames section Finish

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,9 +1,93 @@
 import React from "react";
-import styles from './Footer.module.css'; //be aware when using element styling
+import styles from './module.css'; //be aware when using element styling
 
 function Footer() {
 return (
-<div>Footer    Hashem & Hamza Code here</div>
+
+    <footer>
+      <div className="container">
+        <div className="left-right-side">
+          <div className="left-side">
+            <h3>Contact Us</h3>
+
+            <div className="contact">
+              <div className="icons">
+                <i className="bx bxs-envelope"></i>
+                <span>IGDB@gmail.com</span>
+              </div>
+              <div className="icons">
+                <i className="bx bxs-phone"></i>
+                <span>+91 91813 23 2309</span>
+              </div>
+              <div className="icons">
+                <i className="bx bxs-location-plus"></i>
+                <span>Palestine</span>
+              </div>
+            </div>
+          </div>
+          <div className="right-side">
+            <div className="footer-list">
+              <h4>Home</h4>
+              <ul>
+                <li>
+                  <a href="#">Sign in</a>
+                </li>
+                <li>
+                  <a href="#">Games</a>
+                </li>
+                <li>
+                  <a href="#">Popular games</a>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-list">
+              <h4>
+                <a href="about_us.html#About_Us">About Us</a>
+              </h4>
+              <ul>
+                <li>
+                  <a href="about_us.html#AI_Powered_Insights">
+                    AI-Powered Insights
+                  </a>
+                </li>
+                <li>
+                  <a href="about_us.html#Our_Goals">Our Goals</a>
+                </li>
+                <li>
+                  <a href="about_us.html#What_We_Offer">What We Offer</a>
+                </li>
+                <li>
+                  <a href="about_us.html#Share_Your_Feedback">
+                    Share Your Feedback
+                  </a>
+                </li>
+                <li>
+                  <a href="about_us.html#How_We_Review_Games">
+                    How We Review Games
+                  </a>
+                </li>
+                <li>
+                  <a href="about_us.html#The_World_of_Games">
+                    The World of Games
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-list">
+              <h4>Social Profiles</h4>
+              <div className="icon">
+                <i className="bx bxl-facebook"></i>
+                <i className="bx bxl-twitter"></i>
+                <i className="bx bxl-linkedin"></i>
+                <i className="bx bxl-instagram"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <h6>MadeByGroup7@web.com</h6>
+      </div>
+    </footer>
 
 
 );
