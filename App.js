3ac083bcbe10b7ee3c4eaf3533fb3023e0c73795
hashemsayel <hashemsayel4@gmail.com>
2025-01-04 17:53:36 +0300

    Populargames Section Finish

--- App.js
+++ App.js
@@ -6,41 +6,19 @@ import Header from './components/Header/Header';
 import Signin from './pages/SignIn/Signin';
 import Aboutus from "./pages/AboutUs/Aboutus";
 import Footer from "./components/Footer/Footer";
-import Populargames from "./components/Populargames/Populargames"; // استيراد مكون Populargames
+import Populargames from "./components/Populargames/Populargames"; 
 
 // Don't change anything in the App function without notifying the whole team.
 function App() {
-  const games = [
-    { 
-      image: "image/fortnite.jpg", 
-      name: "Fortnite", 
-      genre: "survival", 
-      rating: "9.5/10" 
-    },
-    { 
-      image: "image/rdr2.jpg", 
-      name: "Rdr2", 
-      genre: "survival", 
-      rating: "9.1/10" 
-    },
-    { 
-      image: "image/battlefield.jpg", 
-      name: "Battlefield", 
-      genre: "war", 
-      rating: "9.9/10" 
-    },
-    { 
-      image: "image/apex.jpg", 
-      name: "Apex Legends", 
-      genre: "survival", 
-      rating: "9.9/10" 
-    },
-    { 
-      image: "image/forza.jpg", 
-      name: "Forza", 
-      genre: "racing", 
-      rating: "9.6/10" 
-    }
+  const names = ["Fortnite", "Rdr2", "Battlefield", "Apex Legends", "Forza"];
+  const genres = ["survival", "survival", "war", "survival", "racing"];
+  const ratings = ["9.5/10", "9.1/10", "9.9/10", "9.9/10", "9.6/10"];
+  const images = [
+    "image/fortnite.jpg",
+    "image/rdr2.jpg",
+    "image/battlefield.jpg",
+    "image/apex.jpg",
+    "image/forza.jpg"
   ];
 
   return (
--- components/Populargames/Populargames.js
+++ components/Populargames/Populargames.js
@@ -1,40 +1,39 @@
 import React, { useState, useEffect } from "react";
 import styles from './Populargames.module.css';
-
-function Populargames({ games }) {  
+function Populargames({ names, genres, ratings, images }) {  
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % games.length);   
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % names.length);  
     }, 3000);
 
-    return () => clearInterval(interval);
-  }, [games.length]);   
-
+    return () => clearInterval(interval); 
+  }, [names.length]);  
   return (
     <section className={styles.Populargames}>
-      <h2>Most Popular</h2>
+    <h2>Most Popular</h2>
 
-      <div className={styles.PopularCards}>
-        {games.map((game, index) => (  
-          <div
-            key={index}
-            className={`${styles.PopularCard} ${currentIndex === index ? styles.active : ''}`}
-          >
-            <div className={styles.Populartext}>
-              <img src={game.image} alt={game.name} />
-              <h3>{game.name}</h3>
-              <span>{game.genre}</span>
-              <div className={styles.rating}>
-                <span>{game.rating}</span>
-                <i className='bx bxs-star bx-flashing bx-flip-horizontal'></i>
-              </div>
+    <div className={styles.PopularCards}>
+      {names.map((name, index) => (  
+        <div 
+          key={index}
+          className={`${styles.PopularCard} ${currentIndex === index ? styles.active : ''}`}
+        >
+          <div className={styles.Populartext}>
+            <img src={images[index]} alt={name} />
+            <h3>{name}</h3>
+            <span>{genres[index]}</span>
+            <div className={styles.rating}>
+              <span>{ratings[index]}</span>
+              <i className='bx bxs-star bx-flashing bx-flip-horizontal'></i>
             </div>
           </div>
-        ))}
-      </div>
-    </section>
+        </div>
+      ))}
+    </div>
+  </section>
+   
   );
 }
 
